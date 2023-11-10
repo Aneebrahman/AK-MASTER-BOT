be87eb518c43c53497d02201979d1a6818feae4f
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-11-10 20:28:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,6 @@ from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from aiohttp import web
-from plugins import web_server
 import time
 from datetime import date, datetime 
 import pytz
@@ -64,11 +62,7 @@ class Bot(Client):
         temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        self.username = '@' + me.username        
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         tz = pytz.timezone('Asia/Kolkata')
         today = date.today()
