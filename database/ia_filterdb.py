0cc5bc54ecdf044d36c5e9ebed82c08234775ae0
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-03-30 14:27:54 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -268,7 +268,6 @@ async def delete_files_below_threshold(db, threshold_size_mb: int = 40, batch_si
 
     
 async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
-async def get_search_results(query, file_type=None, max_results=8, offset=0, filter=False):
     """For given query return (results, next_offset)"""
 
     query = query.strip()
